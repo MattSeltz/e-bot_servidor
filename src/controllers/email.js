0b0857b44f8c551387dd7ce58ea873390d9851d5
Matias Seltzer <107654917+MattSeltz@users.noreply.github.com>
2024-10-14 23:34:30 -0300

    v2

--- src/controllers/email.js
+++ src/controllers/email.js
@@ -4,9 +4,13 @@ export const postData = async (req, res) => {
   const {sender,to,subject,content} = req.body
 
   try {
-    const result = await main(sender,to,subject,content)
+    const result = await main(sender, to, subject, content)
 
-    res.status(200).json(result)
+    if (result) {
+      res.status(200).json(result)
+    } else {
+      res.status(500).json("Error")
+    }
   } catch (error) {
     res.status(500).json({error:error.message})
   }
